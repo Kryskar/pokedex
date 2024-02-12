0211995da6050792c765aa4a9aefa27ebf9d82f9
Kryskar <132471940+Kryskar@users.noreply.github.com>
2024-02-12 14:33:14 +0100

    search prototype working

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,69 @@
-import React, { useRef, useState } from 'react';
-import { getData, useFetchPokemon } from '../api/api';
-import Pokemon from './components/Pokemon';
-
+import React, { useRef, useState } from "react";
+import { getData, useFetchPokemon } from "../api/api";
+import Pokemon from "./components/Pokemon";
 
 const Home: React.FC = () => {
-    const [pokemonNum,setPokemonNum] = useState(20)
-    const [selectValue, setSelectValue] = useState("name")
-    const {pokemonArr} = useFetchPokemon(pokemonNum)
-    
+  const [pokemonNum, setPokemonNum] = useState(20);
+  const [selectValue, setSelectValue] = useState("name");
+  const [search, setSearch] = useState("");
+  const { pokemonArr } = useFetchPokemon(pokemonNum);
 
-   const handleChange = (e:React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectValue(e.target.value)
-   }
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectValue(e.target.value);
+  };
+
+  const pokemonsFiltered = pokemonArr.filter((pokemon) =>
+    selectValue === "name"
+      ? pokemon.name.includes(search)
+      : pokemon.types.some((type) => type.type.name.includes(search))
+  );
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
 
   return (
     <>
-    <input placeholder={`filter pokemons by ${selectValue}`}/><select onChange={(e)=>handleChange(e)}><option value="name">name</option><option value="type">type</option></select>
-    <div>
-    <h1>Pokemon List</h1>
-    
-    <div style={{display:"grid", gridTemplateColumns:"repeat(5,1fr)", gap:"10px"}}>
-      {pokemonArr.map(pokemon => (
-        <Pokemon pokemon={pokemon}/>
-      ))}
-    </div>
-    <br/>
-    <button onClick={() => setPokemonNum(pokemonNum+20)}>Load more</button> 
-    <button disabled={pokemonNum<=20} onClick={() => setPokemonNum(pokemonNum-20)}>Hide</button> 
-  </div>
-  </>
+      <input
+        type="search"
+        onChange={handleInputChange}
+        placeholder={`filter pokemons by ${selectValue}`}
+      />
+      <select onChange={handleSelectChange}>
+        <option value="name">name</option>
+        <option value="type">type</option>
+      </select>
+      <div>
+        <h1>Pokemon List</h1>
+
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(5,1fr)",
+            gap: "10px",
+          }}
+        >
+          {pokemonsFiltered.map((pokemon) => (
+            <Pokemon pokemon={pokemon} />
+          ))}
+        </div>
+
+        {search === "" && (
+          <>
+            <br />
+            <button onClick={() => setPokemonNum(pokemonNum + 20)}>
+              Load more
+            </button>
+            <button
+              disabled={pokemonNum <= 20}
+              onClick={() => setPokemonNum(pokemonNum - 20)}
+            >
+              Hide
+            </button>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
